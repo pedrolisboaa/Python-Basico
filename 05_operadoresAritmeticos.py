407604c3c433198d07bb4d3784c3d201570d940c
Pedro Lisboa <phlisboa2000@gmail.com>
2021-08-11 09:57:56 -0300

    Quando voltar do trabalho continuo estudante.

---
+++ 05_operadoresAritmeticos.py
@@ -0,0 +1,15 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+
+print('Multiplicação', 10*10)
+print('Adição', 10+10)
+print('Subtração', 10-10)
+print('Divisão', 10 / 2)
+print('Divisao inteira', 10 // 3)
+print('Exponenciação', 10 ** 5)
+print('Módulo ou resto da divisão!  ', 10 % 3)
+
+print('Com precedencia', (10 + 5) * 10)
+print('Sem precedencia', 10 + 5 * 10)
+
