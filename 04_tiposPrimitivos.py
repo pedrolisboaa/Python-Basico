8fde32bcdf6c28579622914adb7bce12138ba335
Pedro Lisboa <phlisboa2000@gmail.com>
2021-08-11 09:45:20 -0300

    Atualizando

---
+++ 04_tiposPrimitivos.py
@@ -0,0 +1,21 @@
+"""
+Tipos de dados primitivos
+str - string
+int - inteiro
+float - real/ponto flutuante
+bool - booleano/lógico
+"""
+"""
+print(type('Pedro'), 'Pedro')
+print(type(1234), 1234)
+print(type(10.15), 10.15)
+print(type(10 != 10), 10 != 10)
+print(type(10 == 10), 10 == 10)
+"""
+
+print('Pedro', type('Pedro'))
+print(28, type(28))
+print(1.90, type(1.90))
+print(28 >= 18, type(28 >= 18))
+
+
