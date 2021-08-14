8c348e6ad58a4f6e8a2637098f87dc57408128f4
Pedro Lisboa <47804942+pedrolisboaa@users.noreply.github.com>
2021-08-13 21:06:51 -0300

    Update 13_tipos_embutidos.py

--- 13_tipos_embutidos.py
+++ 13_tipos_embutidos.py
@@ -1,1 +1,11 @@
+num1 = input('Digite um número: ')
+num2 = input('Digite outro número: ')
 
+#isnumeric isdigit isdecimal
+
+#isnumeric - somente positivos inteiros 
+
+if num1.isdigit() and num2.isdigit():
+	print(int(num1) + int(num2))
+else:
+	print('Erro!')
